e9536323b3f26f83011b6fcac43cf9bb648db008
kaoszwerg <zwerch@zwerch.net>
2021-04-28 08:16:12 +0200

    x and y step changed

--- firmware/Marlin-2.0.x/Marlin/Configuration.h
+++ firmware/Marlin-2.0.x/Marlin/Configuration.h
@@ -745,7 +745,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 300, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 300, 500 } //K8//
 
 /**
  * Default Max Feed Rate (mm/s)

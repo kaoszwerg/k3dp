bab42531db98618fa9b67ed244dbb3bd20654c32
kaoszwerg <zwerch@zwerch.net>
2021-05-01 17:17:45 +0200

    extruder steps

--- firmware/Marlin-2.0.x/Marlin/Configuration.h
+++ firmware/Marlin-2.0.x/Marlin/Configuration.h
@@ -745,7 +745,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 300, 80 } //K8//
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 300, 155.28 } //K8//
 
 /**
  * Default Max Feed Rate (mm/s)
